b0444198b4e5ec6ca2c4f2f23f7785ebdc1862f4
ValeriyKozyar <monqpepers@gmail.com>
2020-03-18 15:54:26 +0300

    new

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 # сайт, по которому ведется поиск
 URL = r"https://pythonworld.ru"
+
+DB_config = dict(dbname='postgres', user='postgres', password='wYz3Co97', host='localhost')
--- scraper_url_part1.py
+++ scraper_url_part1.py
@@ -1,50 +1,84 @@
+from collections import deque
+import datetime
+from threading import Thread, Lock
+import logging
+
+import psycopg2
 from bs4 import BeautifulSoup
 import requests
-from collections import deque
 
-from config import URL
+from config import URL, DB_config
 
-all_links = dict()
+# all_links = dict()
+LINKS_IN_SESSION = []
+ALL_LINKS = []
 links_queue = deque()
+LOCK = Lock()
+logging.basicConfig(filename="task_1.log", level=logging.DEBUG, filemode="w")
 
 
-def get_all_link():
-    page = requests.get(URL)
+def save_to_db(conn, url: str, text: str):
+    """ Функция для сохранения страницы в БД """
+    LOCK.acquire()
+    ins = "insert into url_to_topic values('{0}', '{1}', TIMESTAMP '{2}')".format(url, text,
+                                                                                  str(datetime.datetime.today()))
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(ins)
+        conn.commit()
+        logging.DEBUG("Запись в БД - ОК!")
+    except Exception as err:
+        print(f"ERROR:")
+        print(err)
+        print(url)
+        print(text)
+        logging.error(err)
+        conn.rollback()
+        exit(1)
+    finally:
+        LOCK.release()
+
+
+def get_all_link(conn, url: str):
+    if url in LINKS_IN_SESSION:
+        return
+    page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
-    all_links[URL] = soup.get_text()
-    for link in set(soup.findAll('a')):
+    if url not in ALL_LINKS:
+        thread = Thread(target=save_to_db, args=(conn, url, soup.get_text().replace('\'', '"')))
+        thread.setDaemon(True)
+        thread.start()
+        ALL_LINKS.append(url)
+    LINKS_IN_SESSION.append(url)
+    for link in set(soup.findAll("a")):
         try:
-            text_link = str(link['href'])
+            text_link = str(link["href"])
             if not text_link.startswith(('/', URL)):
                 continue
             text_link = text_link if text_link.startswith(URL) else "".join([URL, text_link])
             text_link = text_link.rstrip('/')
-            if text_link not in all_links:
-                links_queue.append(text_link)
-            print(text_link)
+            if text_link not in LINKS_IN_SESSION:
+                get_all_link(conn, text_link)
+                # links_queue.append(text_link)
         except KeyError:
             pass
-    print(all_links)
+        except Exception as err:
+            logging.error(err)
 
 
 def main():
-    # получение главной страницы
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.text, 'lxml')
-    all_links[URL] = soup.get_text()
-    for link in set(soup.findAll('a')):
-        try:
-            text_link = str(link['href'])
-            if not text_link.startswith(('/', URL)):
-                continue
-            text_link = text_link if text_link.startswith(URL) else "".join([URL, text_link])
-            text_link = text_link.rstrip('/')
-            if text_link not in all_links:
-                links_queue.append(text_link)
-            print(text_link)
-        except KeyError:
-            pass
-    print(all_links)
+    global ALL_LINKS
+    conn = None
+    try:
+        conn = psycopg2.connect(**DB_config)
+        with conn.cursor() as cursor:
+            cursor.execute("select url from url_to_topic")
+            links = cursor.fetchall()
+        ALL_LINKS = list(link[0] for link in links)
+        get_all_link(conn, URL)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 main()
