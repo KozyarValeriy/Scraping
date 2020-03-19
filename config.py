6ef21021d412b218a241ca9f1f18db091929535e
ValeriyKozyar <monqpepers@gmail.com>
2020-03-19 16:54:31 +0300

    new

--- config.py
+++
@@ -1,4 +0,0 @@
-# сайт, по которому ведется поиск
-URL = r"https://pythonworld.ru"
-
-DB_config = dict(dbname='postgres', user='postgres', password='wYz3Co97', host='localhost')
--- scraper_url_part1.py
+++ scraper_url_part1.py
@@ -1,7 +1,8 @@
-from collections import deque
 import datetime
 from threading import Thread, Lock
 import logging
+import time
+import re
 
 import psycopg2
 from bs4 import BeautifulSoup
@@ -12,54 +13,67 @@ from config import URL, DB_config
 # all_links = dict()
 LINKS_IN_SESSION = []
 ALL_LINKS = []
-links_queue = deque()
 LOCK = Lock()
-logging.basicConfig(filename="task_1.log", level=logging.DEBUG, filemode="w")
+NOT_PAGE = (".png", ".pdf", ".jpeg", ".bmp")
 
 
 def save_to_db(conn, url: str, text: str):
-    """ Функция для сохранения страницы в БД """
+    """ Функция для сохранения страницы в БД
+
+    :param conn: соединение с БД,
+    :param url: url о страницы,
+    :param text: текст страницы.
+    """
+    # Запускаем в отдельном потоке, поэтому ставим мьютекс
     LOCK.acquire()
+    text = text.strip()
+    text = re.sub(r"\n+", r"\n", text)
     ins = "insert into url_to_topic values('{0}', '{1}', TIMESTAMP '{2}')".format(url, text,
                                                                                   str(datetime.datetime.today()))
     try:
         with conn.cursor() as cursor:
             cursor.execute(ins)
         conn.commit()
-        logging.DEBUG("Запись в БД - ОК!")
+        # Добавляем ссылку в скаченные, если не было ошибки
+        ALL_LINKS.append(url)
     except Exception as err:
-        print(f"ERROR:")
-        print(err)
-        print(url)
-        print(text)
         logging.error(err)
-        conn.rollback()
-        exit(1)
+        # conn.rollback()
     finally:
         LOCK.release()
 
 
 def get_all_link(conn, url: str):
+    """ Функция для рекурсивного обхода ссылок на сайте
+
+    :param conn: соединение с БД,
+    :param url: url для поиска других ссылок и получения текста страницы.
+    """
+    # Если в текущей сессии уже был такой url, то возвращаемся, чтобы не обходить его второй раз
     if url in LINKS_IN_SESSION:
         return
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
+    # Если страницу по этому url еще нет в БД, то записываем ее в БД
     if url not in ALL_LINKS:
         thread = Thread(target=save_to_db, args=(conn, url, soup.get_text().replace('\'', '"')))
         thread.setDaemon(True)
         thread.start()
-        ALL_LINKS.append(url)
+    # Добавляем эту ссылку в пройденные в этой сессии
     LINKS_IN_SESSION.append(url)
+    # Обходим все теги <a> на странице
     for link in set(soup.findAll("a")):
         try:
             text_link = str(link["href"])
-            if not text_link.startswith(('/', URL)):
+            # Если это не ссылка на этот же сайт или это не страница, то пропускаем
+            if not text_link.startswith(('/', URL)) or text_link.endswith(NOT_PAGE):
                 continue
+            # Для относительных ссылок добавляем в начало главный url
             text_link = text_link if text_link.startswith(URL) else "".join([URL, text_link])
             text_link = text_link.rstrip('/')
-            if text_link not in LINKS_IN_SESSION:
-                get_all_link(conn, text_link)
-                # links_queue.append(text_link)
+            # if text_link not in LINKS_IN_SESSION:
+            # ызываем функцию для следующего url
+            get_all_link(conn, text_link)
         except KeyError:
             pass
         except Exception as err:
@@ -75,10 +89,23 @@ def main():
             cursor.execute("select url from url_to_topic")
             links = cursor.fetchall()
         ALL_LINKS = list(link[0] for link in links)
+        print(ALL_LINKS)
+        print(len(ALL_LINKS))
         get_all_link(conn, URL)
+        print(LINKS_IN_SESSION)
+        print(len(LINKS_IN_SESSION))
+    except Exception as err:
+        logging.error(err)
+        # if conn is not None:
+        #     conn.rollback()
     finally:
         if conn is not None:
+            # conn.commit()
             conn.close()
 
 
-main()
+if __name__ == "__main__":
+    logging.basicConfig(filename="task_1.log", level=logging.DEBUG, filemode="w")
+    start = time.perf_counter()
+    main()
+    print("Time spent: {0:6.4f} sec".format(time.perf_counter() - start))
